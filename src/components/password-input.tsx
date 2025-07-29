cee681909fd217f91b06df362d6ed5f6857f3cae
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-07-29 19:30:29 -0300

    feat: create password-input component

---
+++ src/components/password-input.tsx
@@ -0,0 +1,33 @@
+import { Eye, EyeOff } from 'lucide-react'
+import { type ComponentProps, useState } from 'react'
+import { cn } from '../utils/cn'
+
+interface PasswordInputProps extends Omit<ComponentProps<'input'>, 'type'> {}
+
+export function PasswordInput({ className, ...props }: PasswordInputProps) {
+	const [isVisible, setIsVisible] = useState(false)
+
+	function toggleVisibility() {
+		setIsVisible(prevState => !prevState)
+	}
+
+	return (
+		<div className='relative'>
+			<input
+				type={isVisible ? 'text' : 'password'}
+				className={cn(
+					'block h-10 w-full rounded-lg border border-zinc-800 pr-12 indent-2 focus:outline-2 focus:outline-cyan-500',
+					className
+				)}
+				{...props}
+			/>
+			<button
+				type='button'
+				onClick={toggleVisibility}
+				className=' -translate-y-1/2 absolute top-1/2 right-2 transform cursor-pointer rounded-full p-2 transition-colors duration-300 hover:bg-zinc-500/20'
+			>
+				{isVisible ? <Eye size={18} /> : <EyeOff size={18} />}
+			</button>
+		</div>
+	)
+}
