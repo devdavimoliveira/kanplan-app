415a2e952f79ca1271d57294ea50bac107759c56
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-07-29 10:27:12 -0300

    feat: add sign-up link to sign-in form

--- src/pages/_auth/sign-in.tsx
+++ src/pages/_auth/sign-in.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { Button } from '../../components/button'
 import { Input } from '../../components/input'
 
@@ -15,10 +15,18 @@ export const Route = createFileRoute('/_auth/sign-in')({
 
 function SignIn() {
 	return (
-		<form className='flex flex-col gap-4'>
-			<Input type='email' placeholder='E-mail' />
-			<Input type='password' placeholder='Senha' />
-			<Button type='submit'>Entrar</Button>
-		</form>
+		<div className='flex flex-col gap-4'>
+			<form className='flex flex-col gap-4'>
+				<Input type='email' placeholder='E-mail' />
+				<Input type='password' placeholder='Senha' />
+				<Button type='submit'>Entrar</Button>
+			</form>
+			<span className='self-center text-sm text-zinc-500'>
+				Não tem uma conta?{' '}
+				<Link to='/sign-up' className='text-cyan-500 hover:underline'>
+					Cadastre-se
+				</Link>
+			</span>
+		</div>
 	)
 }
