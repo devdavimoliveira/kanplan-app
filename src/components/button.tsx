911be898dcca4d408a8b3e5aa871f098f790969a
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-07-28 21:15:57 -0300

    feat: create button component

---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+import type { ComponentProps } from 'react'
+import { cn } from '../utils/cn'
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button({ className, ...props }: ButtonProps) {
+	return (
+		<button
+			className={cn(
+				'h-10 rounded-lg border border-cyan-500 font-semibold text-cyan-500 outline-none transition-colors duration-300 hover:bg-cyan-500 hover:text-inherit focus:bg-cyan-500 focus:text-inherit',
+				className
+			)}
+			{...props}
+		/>
+	)
+}
