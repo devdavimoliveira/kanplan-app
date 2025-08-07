8d1d2bb86d1dd319c8d3545492fb79efacafd9c2
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-06 22:24:25 -0300

    chore: setup organization plugin in auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,16 @@
+import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_URL,
   basePath: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_PATH,
+  plugins: [
+    organizationClient({
+      teams: {
+        enabled: true,
+      },
+    }),
+  ],
 });
 
 export const { useSession, signIn, signUp, signOut, getSession } = authClient;
