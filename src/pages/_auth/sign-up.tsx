f73bb2bca1f16e939117aa0def7d70ad3576cdd0
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-07 18:01:42 -0300

    feat: implement sign-up function on sign-up page

--- src/pages/_auth/sign-up.tsx
+++ src/pages/_auth/sign-up.tsx
@@ -1,4 +1,11 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { createFileRoute, Link } from '@tanstack/react-router'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z from 'zod'
+import { signUp } from '@/lib/auth-client'
+import { cn } from '@/utils/cn'
+import { getAuthErrorMessage } from '@/utils/get-auth-error-message'
 import { Button } from '../../components/button'
 import { Input } from '../../components/input'
 import { PasswordInput } from '../../components/password-input'
@@ -14,14 +21,88 @@ export const Route = createFileRoute('/_auth/sign-up')({
 	}),
 })
 
+const signUpSchema = z.object({
+	name: z.string().nonempty('Insira um nome'),
+	email: z.email('Insira um e-mail válido'),
+	password: z.string().nonempty('Insira uma senha'),
+})
+
+type SignUpFormType = z.infer<typeof signUpSchema>
+
 function SignUp() {
+	const navigate = Route.useNavigate()
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<SignUpFormType>({
+		resolver: zodResolver(signUpSchema),
+	})
+
+	const isInvalidName = !!errors.name
+	const isInvalidEmail = !!errors.email
+	const isInvalidPassword = !!errors.password
+
+	async function handleSignUp({ name, email, password }: SignUpFormType) {
+		await signUp.email({
+			name,
+			email,
+			password,
+			fetchOptions: {
+				onError(ctx) {
+					toast.error(getAuthErrorMessage(ctx.error.code))
+				},
+				onSuccess: () => navigate({ to: '/', replace: true }),
+			},
+		})
+	}
+
 	return (
 		<div className='flex flex-col gap-4'>
-			<form className='flex flex-col gap-4'>
-				<Input type='text' placeholder='Nome completo' />
-				<Input type='email' placeholder='E-mail' />
-				<PasswordInput placeholder='Senha' />
-				<Button type='submit'>Criar conta</Button>
+			<form
+				className='flex flex-col gap-4'
+				onSubmit={handleSubmit(handleSignUp)}
+			>
+				<div className='flex flex-col gap-1'>
+					<Input
+						type='text'
+						placeholder='Nome completo'
+						className={cn(isInvalidName && 'outline-2 outline-red-500')}
+						{...register('name')}
+					/>
+					{errors.name && (
+						<span className='ml-2 text-red-500'>{errors.name.message}</span>
+					)}
+				</div>
+				<div className='flex flex-col gap-1'>
+					<Input
+						type='email'
+						placeholder='E-mail'
+						{...register('email')}
+						className={cn(isInvalidEmail && 'outline-2 outline-red-500')}
+					/>
+					{errors.email && (
+						<span className='ml-2 text-red-500'>{errors.email.message}</span>
+					)}
+				</div>
+				<div className='flex flex-col gap-1'>
+					<PasswordInput
+						placeholder='Senha'
+						{...register('password')}
+						className={cn(isInvalidPassword && 'outline-2 outline-red-500')}
+					/>
+					{errors.password && (
+						<span className='ml-2 text-red-500'>{errors.password.message}</span>
+					)}
+				</div>
+				<Button
+					type='submit'
+					disabled={isSubmitting}
+					className='disabled:pointer-events-auto disabled:cursor-wait'
+				>
+					Criar conta
+				</Button>
 			</form>
 			<span className='self-center text-sm text-zinc-500'>
 				Já tem uma conta?{' '}
