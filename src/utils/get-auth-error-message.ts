5245c976d6d0cd8401a7823a75ce824c5f12626a
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-08 00:53:39 -0300

    feat: update errors in get auth error message function

--- src/utils/get-auth-error-message.ts
+++ src/utils/get-auth-error-message.ts
@@ -82,6 +82,130 @@ const errorCodes = {
   USER_EMAIL_NOT_FOUND: {
     message: "E-mail do usuário não encontrado",
   },
+  FAILED_TO_RETRIEVE_INVITATION: {
+    message: "Falha ao recuperar convite",
+  },
+  INVITATION_LIMIT_REACHED: {
+    message: "Limite de convites atingido",
+  },
+  INVITATION_NOT_FOUND: {
+    message: "Convite não encontrado",
+  },
+  INVITER_IS_NO_LONGER_A_MEMBER_OF_THE_ORGANIZATION: {
+    message: "O convidado não é mais um membro da organização",
+  },
+  MEMBER_NOT_FOUND: {
+    message: "Membro não encontrado",
+  },
+  NO_ACTIVE_ORGANIZATION: {
+    message: "Nenhuma organização ativa",
+  },
+  ORGANIZATION_ALREADY_EXISTS: {
+    message: "Organização já existe",
+  },
+  ORGANIZATION_MEMBERSHIP_LIMIT_REACHED: {
+    message: "Limite de membros da organização atingido",
+  },
+  ORGANIZATION_NOT_FOUND: {
+    message: "Organização não encontrada",
+  },
+  ROLE_NOT_FOUND: {
+    message: "Cargo não encontrada",
+  },
+  TEAM_ALREADY_EXISTS: {
+    message: "Equipe já existe",
+  },
+  TEAM_MEMBER_LIMIT_REACHED: {
+    message: "Limite de membros da equipe atingido",
+  },
+  TEAM_NOT_FOUND: {
+    message: "Equipe não encontrada",
+  },
+  UNABLE_TO_REMOVE_LAST_TEAM: {
+    message: "Não é possível remover a última equipe",
+  },
+  USER_IS_ALREADY_A_MEMBER_OF_THIS_ORGANIZATION: {
+    message: "O usuário já é um membro desta organização",
+  },
+  USER_IS_ALREADY_INVITED_TO_THIS_ORGANIZATION: {
+    message: "O usuário já foi convidado para esta organização",
+  },
+  USER_IS_NOT_A_MEMBER_OF_THE_ORGANIZATION: {
+    message: "O usuário não é um membro da organização",
+  },
+  USER_IS_NOT_A_MEMBER_OF_THE_TEAM: {
+    message: "O usuário não é um membro da equipe",
+  },
+  YOU_ARE_NOT_A_MEMBER_OF_THIS_ORGANIZATION: {
+    message: "Você não é um membro desta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_ACCESS_THIS_ORGANIZATION: {
+    message: "Você não tem permissão para acessar esta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_CANCEL_THIS_INVITATION: {
+    message: "Você não tem permissão para cancelar este convite",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_CREATE_A_NEW_ORGANIZATION: {
+    message: "Você não tem permissão para criar uma nova organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_CREATE_A_NEW_TEAM: {
+    message: "Você não tem permissão para criar uma nova equipe",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_CREATE_A_NEW_TEAM_MEMBER: {
+    message: "Você não tem permissão para criar um novo membro da equipe",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_CREATE_TEAMS_IN_THIS_ORGANIZATION: {
+    message: "Você não tem permissão para criar equipes nesta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_DELETE_TEAMS_IN_THIS_ORGANIZATION: {
+    message: "Você não tem permissão para excluir equipes nesta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_DELETE_THIS_MEMBER: {
+    message: "Você não tem permissão para excluir este membro",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_DELETE_THIS_ORGANIZATION: {
+    message: "Você não tem permissão para excluir esta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_DELETE_THIS_TEAM: {
+    message: "Você não tem permissão para excluir esta equipe",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_INVITE_USER_WITH_THIS_ROLE: {
+    message: "Você não tem permissão para convidar usuários com este cargo",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_INVITE_USERS_TO_THIS_ORGANIZATION: {
+    message:
+      "Você não tem permissão para convidar usuários para esta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_REMOVE_A_TEAM_MEMBER: {
+    message: "Você não tem permissão para remover um membro da equipe",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_UPDATE_THIS_MEMBER: {
+    message: "Você não tem permissão para atualizar este membro",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_UPDATE_THIS_ORGANIZATION: {
+    message: "Você não tem permissão para atualizar esta organização",
+  },
+  YOU_ARE_NOT_ALLOWED_TO_UPDATE_THIS_TEAM: {
+    message: "Você não tem permissão para atualizar esta equipe",
+  },
+  YOU_ARE_NOT_THE_RECIPIENT_OF_THE_INVITATION: {
+    message: "Você não é o destinatário do convite",
+  },
+  YOU_CAN_NOT_ACCESS_THE_MEMBERS_OF_THIS_TEAM: {
+    message: "Você não pode acessar os membros desta equipe",
+  },
+  YOU_CANNOT_LEAVE_THE_ORGANIZATION_AS_THE_ONLY_OWNER: {
+    message: "Você não pode sair da organização pois é o único proprietário",
+  },
+  YOU_DO_NOT_HAVE_AN_ACTIVE_TEAM: {
+    message: "Você não tem uma equipe ativa",
+  },
+  YOU_HAVE_REACHED_THE_MAXIMUM_NUMBER_OF_ORGANIZATIONS: {
+    message: "Você atingiu o número máximo de organizações",
+  },
+  YOU_HAVE_REACHED_THE_MAXIMUM_NUMBER_OF_TEAMS: {
+    message: "Você atingiu o número máximo de equipes",
+  },
 } satisfies ErrorTypes;
 
 export const getAuthErrorMessage = (code: string) => {
