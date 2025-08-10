b87759f57531c4b4b71dc8357cf8c78737317c48
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-09 22:52:45 -0300

    feat: implement tanstack react-router-devtools

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,4 +1,5 @@
 import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
 	component: RootComponent,
@@ -9,6 +10,7 @@ function RootComponent() {
 		<>
 			<HeadContent />
 			<Outlet />
+			<TanStackRouterDevtools position='bottom-left' />
 		</>
 	)
 }
