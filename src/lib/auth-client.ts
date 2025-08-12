1ef45b7f044c98daad53c0b3b0ced5e58500dbcb
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-11 23:43:37 -0300

    chore: remove teams from the organization plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,13 +4,7 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_URL,
   basePath: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_PATH,
-  plugins: [
-    organizationClient({
-      teams: {
-        enabled: true,
-      },
-    }),
-  ],
+  plugins: [organizationClient()],
 });
 
 export const {
