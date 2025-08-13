2f8ba9c97ba0c897a0a966d6c646ffdb94a7caaf
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-12 22:44:08 -0300

    feat: set active organization when entering organization page

--- src/pages/_app/_organization-set/org/$orgSlug.tsx
+++ src/pages/_app/_organization-set/org/$orgSlug.tsx
@@ -1,14 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useActiveOrganization } from '@/lib/auth-client'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { toast } from 'sonner'
+import { organization } from '@/lib/auth-client'
+import { getAuthErrorMessage } from '@/utils/get-auth-error-message'
 
 export const Route = createFileRoute('/_app/_organization-set/org/$orgSlug')({
+	beforeLoad: async ({ params }) => {
+		const { data: activeOrganization } = await organization.getFullOrganization(
+			{
+				query: { organizationSlug: params.orgSlug, membersLimit: 0 },
+				fetchOptions: {
+					onError: ({ error }) => {
+						toast.error(
+							`Organização não existe ou ${getAuthErrorMessage(error.code).toLowerCase()}`
+						)
+						throw redirect({ to: '/organizations' })
+					},
+				},
+			}
+		)
+
+		if (activeOrganization) {
+			await organization.setActive({
+				organizationId: activeOrganization!.id,
+			})
+		}
+
+		return {
+			activeOrganization,
+		}
+	},
 	component: Organization,
 })
 
 function Organization() {
-	const { data, isPending } = useActiveOrganization()
+	const { activeOrganization } = Route.useRouteContext()
 
-	if (isPending) return <div>Carregando...</div>
-
-	return <div>{data?.name}</div>
+	return <div>{activeOrganization?.name}</div>
 }
