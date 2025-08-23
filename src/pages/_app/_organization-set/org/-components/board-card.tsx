3d9d835f2af049005276309110d515fffdf8e18f
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-23 00:15:54 -0300

    feat: add highlight color field to board type

--- src/pages/_app/_organization-set/org/-components/board-card.tsx
+++ src/pages/_app/_organization-set/org/-components/board-card.tsx
@@ -9,7 +9,10 @@ export function BoardCard({ board }: BoardCardProps) {
 	return (
 		<Link to='/'>
 			<div className='flex h-30 flex-col rounded-lg bg-zinc-900 shadow-card'>
-				<div className='h-full rounded-t-lg bg-green-500' />
+				<div
+					className='h-full rounded-t-lg'
+					style={{ backgroundColor: board.highlightColor }}
+				/>
 				<span className='h-10 shrink-0 p-2'>{board.name}</span>
 			</div>
 		</Link>
--- src/types/Board.ts
+++ src/types/Board.ts
@@ -1,6 +1,7 @@
 export type Board = {
   id: string;
   name: string;
+  highlightColor: string;
   organizationId: string;
   createdAt: string | null;
 };
