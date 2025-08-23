f8aecbdefb593bc1a77db771be71133d28b31107
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-22 23:33:03 -0300

    feat: create new-board-dialog component

--- src/pages/_app/_organization-set/org/$orgSlug.tsx
+++ src/pages/_app/_organization-set/org/$orgSlug.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 import { toast } from 'sonner'
+import { Button } from '@/components/button'
 import { organization } from '@/lib/auth-client'
 import { getAuthErrorMessage } from '@/utils/get-auth-error-message'
 import { BoardsGrid } from './-components/boards-grid'
+import { NewBoardDialog } from './-components/new-board-dialog'
 
 export const Route = createFileRoute('/_app/_organization-set/org/$orgSlug')({
 	beforeLoad: async ({ params }) => {
@@ -39,6 +41,12 @@ function Organization() {
 	return (
 		<div className='mx-auto flex max-w-5xl flex-col gap-8 py-8'>
 			<h1 className='font-medium text-2xl'>Quadros</h1>
+
+			<NewBoardDialog
+				trigger={<Button className='h-8 w-fit px-2'>Novo quadro</Button>}
+				organization={activeOrganization!}
+			/>
+
 			<BoardsGrid organizationId={activeOrganization!.id} />
 		</div>
 	)
---
+++ src/pages/_app/_organization-set/org/-components/new-board-dialog.tsx
@@ -0,0 +1,102 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as Dialog from '@radix-ui/react-dialog'
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
+import type { ReactNode } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import z from 'zod'
+import { Button } from '@/components/button'
+import { ColorPicker } from '@/components/color-picker'
+import { Input } from '@/components/input'
+import type { Organization } from '@/types/Organization'
+import { cn } from '@/utils/cn'
+
+const DEFAULT_COLOR = '#0092B8'
+
+const newBoardSchema = z.object({
+	name: z.string().nonempty('Insira um nome'),
+	highlightColor: z.string().default(DEFAULT_COLOR),
+	organizationId: z.string(),
+})
+
+type NewBoardFormType = z.infer<typeof newBoardSchema>
+
+interface NewBoardDialogProps {
+	trigger: ReactNode
+	organization: Organization
+}
+
+export function NewBoardDialog({ trigger, organization }: NewBoardDialogProps) {
+	const {
+		handleSubmit,
+		register,
+		control,
+		reset,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		resolver: zodResolver(newBoardSchema),
+		defaultValues: {
+			name: '',
+			highlightColor: DEFAULT_COLOR,
+			organizationId: organization.id,
+		},
+	})
+
+	function handleNewBoard(data: NewBoardFormType) {
+		console.log(data)
+		reset()
+	}
+
+	return (
+		<Dialog.Root>
+			<Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+
+			<Dialog.Portal>
+				<Dialog.Overlay className='fixed inset-0 bg-black/50 backdrop-blur-xs' />
+				<Dialog.Content className='-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 w-[90vw] max-w-lg transform rounded-lg bg-zinc-900 shadow-card'>
+					<Dialog.Title className='p-4 font-medium'>
+						Criar um novo quadro
+					</Dialog.Title>
+
+					<VisuallyHidden>
+						<Dialog.Description>
+							{`Criar um novo quadro dentro da organização ${organization.name}`}
+						</Dialog.Description>
+					</VisuallyHidden>
+
+					<div className='h-px bg-zinc-500' />
+
+					<form
+						onSubmit={handleSubmit(handleNewBoard)}
+						className='flex flex-col gap-4 p-4'
+					>
+						<div className='flex flex-col gap-1'>
+							<Input
+								placeholder='Nome do quadro'
+								className={cn(!!errors.name && 'outline-2 outline-red-500')}
+								{...register('name')}
+							/>
+							{errors.name && (
+								<span className='ml-2 text-red-500'>{errors.name.message}</span>
+							)}
+						</div>
+
+						<div className='flex items-center gap-2 self-end'>
+							<span>Cor de destaque</span>
+							<Controller
+								name='highlightColor'
+								control={control}
+								render={({ field: { onChange, value } }) => (
+									<ColorPicker color={value} onChange={onChange} align='end' />
+								)}
+							/>
+						</div>
+
+						<Button type='submit' disabled={isSubmitting}>
+							Criar quadro
+						</Button>
+					</form>
+				</Dialog.Content>
+			</Dialog.Portal>
+		</Dialog.Root>
+	)
+}
