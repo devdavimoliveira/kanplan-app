4712983ff9db98f5d514bc5a796787245b01ae44
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-08-26 15:36:46 -0300

    feat: add create-board api function

---
+++ src/api/boards/create-board.ts
@@ -0,0 +1,14 @@
+import { api } from "@/lib/axios";
+import type { Board } from "@/types/Board";
+
+export interface CreateBoardBody {
+  name: string;
+  highlightColor: string;
+  organizationId: string;
+}
+
+export async function createBoard(body: CreateBoardBody) {
+  const { data } = await api.post<Board>("/boards/create", body);
+
+  return data;
+}
