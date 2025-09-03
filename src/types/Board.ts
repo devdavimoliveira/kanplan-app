d9ddbc470d69605a252993033cced701f3ec6da7
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-09-03 15:59:53 -0300

    feat: create column type and task type

--- src/types/Board.ts
+++ src/types/Board.ts
@@ -1,7 +1,13 @@
+import type { ColumnWithTasks } from "./Column";
+
 export type Board = {
   id: string;
   name: string;
   highlightColor: string;
   organizationId: string;
   createdAt: string | null;
+};
+
+export type BoardWithColumnsAndTasks = Board & {
+  columns: ColumnWithTasks[];
 };
---
+++ src/types/Task.ts
@@ -0,0 +1,7 @@
+export type Task = {
+  id: string;
+  description: string;
+  position: number;
+  columnId: string;
+  createdAt: string | null;
+};
