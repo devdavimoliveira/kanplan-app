11e52c13d37cd515ecd3d8b4ab4bbdf29cb5d761
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-10-14 19:33:29 -0300

    style: add styles to input component when disabled

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,7 +7,7 @@ export function Input({ className, ...props }: InputProps) {
 	return (
 		<input
 			className={cn(
-				'h-10 rounded-lg border border-zinc-800 indent-2 focus:outline-2 focus:outline-cyan-500',
+				'h-10 rounded-lg border border-zinc-800 indent-2 focus:outline-2 focus:outline-cyan-500 disabled:cursor-not-allowed disabled:text-zinc-500',
 				className
 			)}
 			{...props}
