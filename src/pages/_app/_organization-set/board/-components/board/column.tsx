19d4c73d37c633f4985e905241c58bc8ca8661dd
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-10-27 15:28:09 -0300

    style: remove plus button at top of kanban board columns

--- src/pages/_app/_organization-set/board/-components/board/column.tsx
+++ src/pages/_app/_organization-set/board/-components/board/column.tsx
@@ -25,24 +25,20 @@ export function BoardColumn({ column, highlightColor }: BoardColumnProps) {
 		<div
 			ref={setNodeRef}
 			className={cn(
-				'flex w-75 flex-col gap-4 rounded border-t-5 bg-zinc-900 p-4'
+				'flex w-75 flex-col gap-2 rounded border-t-5 bg-zinc-900 p-4'
 			)}
 			style={{ borderColor: highlightColor ?? colors.cyan[600] }}
 		>
 			<div className='flex items-center justify-between'>
 				<h2 className='font-bold text-xl'>{column.title}</h2>
-				<div className='flex gap-2'>
-					<button
-						type='button'
-						title='Adicionar nova tarefa'
-						className='cursor-pointer'
-					>
-						<Plus size={20} />
-					</button>
-					<button type='button' title='Opções' className='cursor-pointer'>
-						<EllipsisVertical size={20} />
-					</button>
-				</div>
+				<Button
+					variant='ghost'
+					type='button'
+					title='Opções'
+					className='h-auto px-0.5 py-1.5'
+				>
+					<EllipsisVertical size={20} />
+				</Button>
 			</div>
 			<ul className='flex flex-col gap-4'>
 				{column.tasks.map(task => (
