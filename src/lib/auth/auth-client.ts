86c3ed1099b9ca9b335bee1e93b6a6e15b617187
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-12-17 18:27:20 -0300

    refactor: remove access control in the organization client plugin

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,20 +1,10 @@
 import { organizationClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
-import { ac, admin, member, owner } from './permissions'
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_URL,
 	basePath: import.meta.env.VITE_BETTER_AUTH_CLIENT_BASE_PATH,
-	plugins: [
-		organizationClient({
-			ac,
-			roles: {
-				owner,
-				admin,
-				member,
-			},
-		}),
-	],
+	plugins: [organizationClient()],
 })
 
 export const {
