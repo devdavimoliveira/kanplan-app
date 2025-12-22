0b3fe4270f930ec90ee93b636415631ba74f73dd
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-12-22 18:00:14 -0300

    feat: create ability context

---
+++ src/contexts/ability-context.tsx
@@ -0,0 +1,22 @@
+import { defineAbilityFor, type AppAbility } from '@/lib/casl/ability'
+import { createContext, type ReactNode } from 'react'
+import { createContextualCan } from '@casl/react'
+import type { AuthUser } from '@/lib/casl/schemas/auth-user'
+
+export const AbilityContext = createContext({} as AppAbility)
+export const Can = createContextualCan(AbilityContext.Consumer)
+
+interface AbilityProviderProps {
+	children: ReactNode
+	authUser: AuthUser
+}
+
+export function AbilityProvider({ children, authUser }: AbilityProviderProps) {
+	const ability = defineAbilityFor(authUser)
+
+	return (
+		<AbilityContext.Provider value={ability}>
+			{children}
+		</AbilityContext.Provider>
+	)
+}
