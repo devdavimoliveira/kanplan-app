e69d05cb96284ee1673dcec229cfc60a70e050a2
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2025-12-22 19:56:22 -0300

    fix: fix attributes in SVGs

--- src/pages/_auth/-components/social-login.tsx
+++ src/pages/_auth/-components/social-login.tsx
@@ -31,11 +31,11 @@ export function SocialLogin() {
 					xmlns='http://www.w3.org/2000/svg'
 				>
 					<title>Google</title>
-					<g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+					<g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
 					<g
 						id='SVGRepo_tracerCarrier'
-						stroke-linecap='round'
-						stroke-linejoin='round'
+						strokeLinecap='round'
+						strokeLinejoin='round'
 					></g>
 					<g id='SVGRepo_iconCarrier'>
 						{' '}
@@ -56,11 +56,11 @@ export function SocialLogin() {
 					fill='currentColor'
 					className='mr-1 size-8'
 				>
-					<g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+					<g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
 					<g
 						id='SVGRepo_tracerCarrier'
-						stroke-linecap='round'
-						stroke-linejoin='round'
+						strokeLinecap='round'
+						strokeLinejoin='round'
 					></g>
 					<g id='SVGRepo_iconCarrier'>
 						{' '}
