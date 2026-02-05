23c8af230bfd89d34a878010dcc6ced452bc6f96
Davi Machado Oliveira <devdavimoliveira@gmail.com>
2026-02-05 16:34:16 -0300

    refactor: refactor UpdateTaskBody type

--- src/api/tasks/update-task.ts
+++ src/api/tasks/update-task.ts
@@ -1,12 +1,16 @@
 import { api } from '@/lib/axios'
 import type { Task } from '@/types/Task'
+import type { CreateTaskBody } from './create-task'
 
 export interface UpdateTaskParams {
 	taskId: string
 }
 
 export interface UpdateTaskBody
-	extends Omit<Partial<Task>, 'id' | 'createdAt' | 'createdBy'> {}
+	extends Omit<Partial<CreateTaskBody>, 'createdBy'> {
+	markingColor?: string | null
+	assignedBy?: string | null
+}
 
 export async function updateTask(
 	params: UpdateTaskParams,
